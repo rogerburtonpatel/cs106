69049486cccf8e5467da8c83732d978440889f2b
Roger Burtonpatel <roger.burtopatel@tufts.edu>
2023-01-20 01:05:48 -0500

    Changed program counter to literal uint and changed code accordingly. Wrote vmrun for halt.

--- src/svm/vmstate.h
+++ src/svm/vmstate.h
@@ -19,8 +19,8 @@ typedef struct VMState *VMState;
 // ... define the struct type here ...
 
 struct VMState {
-   // instructions-- pointer as a program counter
-    Instruction *counter;
+   // counter-- indexer into instructions array. 
+    uint64_t counter;
    // regs
    Value registers[256];
    // literals- read-only
@@ -30,7 +30,7 @@ struct VMState {
    // store is the heap!
 };
 
-VMState newstate(struct VMFunction *program);       // allocate and initialize (to empty)
+VMState newstate(void);       // allocate and initialize (to empty)
 void freestatep(VMState *sp); // deallocate
 
 
