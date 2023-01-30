d2dc5ccd7e364b8711cd847a8dff14cad1f06141
Roger Burtonpatel <roger.burtopatel@tufts.edu>
2023-01-29 22:23:04 -0500

    copied over get_instruction; todo: global_slot and loadfun

--- src/svm/iparsers.c
+++ src/svm/iparsers.c
@@ -12,6 +12,9 @@
 
 #define SEE(R) do { if ((R) > *maxreg) *maxreg = (R); } while(0)
 
+/* Helper functions */
+static Value get_literal(Tokens *litp, const char *input);
+
 Instruction parseR3(VMState vm, Opcode opcode, Tokens operands, unsigned *maxreg) {
   (void)vm;
   uint8_t regX = tokens_get_byte(&operands, NULL);
@@ -51,7 +54,7 @@ Instruction parseR1U16(VMState vm, Opcode opcode, Tokens operands, unsigned *max
   (void)maxreg;
   uint8_t regX = tokens_get_byte(&operands, NULL);
   uint32_t immediate = tokens_get_int(&operands, NULL);
-  assert(operands == NULL);
+  assert(operands == NULL); // hit the newline
   assert(immediate == (uint16_t) immediate);
   SEE(regX);
   return eR1U16(opcode, regX, immediate);
@@ -89,8 +92,58 @@ static void initnames(void) {
   }
 }
 
+/* Encode load literal instruction */
 Instruction parseR1LIT(VMState vm, Opcode opcode, Tokens operands, unsigned *maxreg) {
   initnames(); // before comparing names, you must call this function
-  (void)vm; (void)opcode; (void)operands; (void)maxreg; // replace with real code!
-  assert(0);
+  uint8_t reg = tokens_get_byte(&operands, NULL);
+  Value litv = get_literal(&operands, NULL);
+  int litslot = literal_slot(vm, litv);
+  assert(litslot == (uint16_t) litslot);
+  assert(operands == NULL);
+  SEE(reg);
+  return eR1U16(opcode, reg, litslot);
+}
+
+/* Encode global variable declaration instruction */
+Instruction parseR1GLO(VMState vm, Opcode opcode, Tokens operands, unsigned *maxreg) {
+  initnames();
+  uint8_t reg = tokens_get_byte(&operands, NULL);
+  Value globalname = get_literal(&operands, NULL);
+  uint32_t globalslot = global_slot(vm, globalname);
+  assert(globalslot == (uint16_t) globalslot);
+  assert(operands == NULL);
+  Instruction instruction = eR1U16(opcode, reg, globalslot);
+  SEE(reg);
+  return instruction;
+}
+
+/* Get Literal Value from the token stream and return corresponding Value */
+static Value get_literal(Tokens *litp, const char *input) {
+
+    int tokenType = first_token_type(*litp);
+    Name name = tokens_get_name(litp, input);
+
+    if (tokenType == TNAME) {
+        if (name == truename) {
+            return mkBooleanValue(true);
+        } else if (name == falsename) {
+            return mkBooleanValue(false);
+        } else if (name == nilname) {
+            return nilValue;
+        } else if (name == emptyname) {
+            return emptylistValue;
+        } else if (name == stringname) {
+            uint32_t len = tokens_get_int(litp, input);
+            StringBuffer sb = Vmstring_buffer(len);
+            for (uint32_t i = 0; i < len; ++i) {
+                Vmstring_putc(sb, tokens_get_byte(litp, input));
+            }
+            VMString vstr = Vmstring_of_buffer(&sb);
+            return mkStringValue(vstr);
+        }
+    } else if (tokenType == TU32) {
+        return mkNumberValue((Number_T) tokens_get_int(litp, input));
+    }
+  // fall-through for TDOUBLE type
+    return mkNumberValue((Number_T) tokens_get_signed_number(litp, input));
 }
