1d886b13dc6f5f6ee02f5d8c1fbf24676618f1f3
Roger Burtonpatel <roger.burtopatel@tufts.edu>
2023-02-14 23:35:42 -0500

    hi liam and sky. right before submitting-- hope everything's right and i don't have to use the unholy git ammend

--- src/svm/instructions.c
+++ src/svm/instructions.c
@@ -36,6 +36,7 @@ instruction_info instructions[] = {
   { ">=",  Ge,  parseR3, "<= rX rY rZ" },
   { "<=",  Le,  parseR3, ">= rX rY rZ" },
   { "cons",  Le,  parseR3, "cons rX rY rZ" },
+  { "=",  Le,  parseR3, "= rX rY rZ" },
 
   { "inc", Inc, parseR1, "inc rX" },
   { "dec", Dec, parseR1, "dec rX" },
@@ -43,7 +44,7 @@ instruction_info instructions[] = {
   { "not", Not, parseR1, "not rX" },
 
   {"boolOf", BoolOf, parseR2, "boolof rX rY" },
-  {"=", RegAssign, parseR2, "rX := rY"},
+  {"assign", RegAssign, parseR2, "rX := rY"},
   {"car", Car, parseR2, "rX := car rY"},
   {"cdr", Cdr, parseR2, "rX := cdr rY"},
   {"function?", IsFunction, parseR2, "rX := function? rY"},
