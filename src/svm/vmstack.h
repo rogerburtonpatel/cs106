621fa4f971c38898410f6cedd59eea4286d22923
Norman Ramsey <nr@cs.tufts.edu>
2023-03-01 09:41:06 -0500

    add vmstack.h; ready for module 7

---
+++ src/svm/vmstack.h
@@ -0,0 +1,11 @@
+#ifndef VMSTACK_INCLUDED
+#define VMSTACK_INCLUDED
+
+#include "value.h"
+
+struct Activation {
+  void *DELETE_ME; // I'm only here so the code will compile
+  // ... you fill in the fields ...
+};
+
+#endif
