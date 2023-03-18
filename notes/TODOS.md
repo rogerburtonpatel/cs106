f2b94168898f88240c7cf244e89e92e0e57edc2f
Roger Burtonpatel <roger.burtonpatel@tufts.edu>
2023-03-17 21:53:57 -0400

    biggen up them nums:

--- notes/TODOS.md
+++ notes/TODOS.md
@@ -1,23 +1,10 @@
-If you want a rant on the topic, ask me. re: parsing. Maybe?
-- depth pts!!! 'the' and explode/implode and others
+# Actual todos
 
-            // case InitConsCell: {
-            //     struct VMBlock *vmb = vmalloc_raw(sizeof(*vmb));
-            //     vmb->nslots = 0;
+Cons cells!!!!
 
-            // }
+Ask about setting globals to literals
 
-Depth:
-Dynamic compilation and loading [2 points]. 
-μScheme and vScheme both have a use syntactic form, which tells the interpreter 
-to load and run code. This goal is to implement two machine instructions, one to
- call popen and read from a pipe, and one to load a list of modules from an open
-  file descriptor. These two instructions can then eventually be used to call 
-  the compiler and load the results. These instructions can be tested now, and 
-  then by the time of module 4, they can be used to implement a use function.
-
-todo:
-set as begin embed
+comment parser broken-- what's wrong with my code?
 
 qs: 
 Projection to KNF should probably reject SETGLOBAL in a value (toReg) context.
@@ -28,6 +15,28 @@ Projection to KNF should probably reject SETGLOBAL in a value (toReg) context.
 knproject.sml:71
 
 
+
+# Depth
+
+# Bignums 
+1. build bignums module 
+   - new, free
+   - add, sub, mult, div, idiv, mod. 
+   - print
+   - ask about floating point
+
+
+Dynamic compilation and loading [2 points]. 
+μScheme and vScheme both have a use syntactic form, which tells the interpreter 
+to load and run code. This goal is to implement two machine instructions, one to
+ call popen and read from a pipe, and one to load a list of modules from an open
+  file descriptor. These two instructions can then eventually be used to call 
+  the compiler and load the results. These instructions can be tested now, and 
+  then by the time of module 4, they can be used to implement a use function.
+
+
+
+# A-Normal Form
 Here's the anormal spiel:
 
 What if we have 
@@ -74,33 +83,34 @@ have illegal forms?
 
 
 
-Cons cells!!!! and clean up Eq and other instrs
 
-
-Ask about setting globals to literals
-
-FOR NORMAN: the goto in the provided vo code: returnt.vo was wrong. 
-goto 5 skips over the instruction. why?
-
-comment parser broken-- what's wrong with my code?
-
-
+# Random todos
 Add <:> operator : curry op cons + <$>
-
 Ask about literal after @: can we have it, bc we use regs?
 
-Any let with more than one binding is not valid. So what are all these let*s
-doing? They got a lot. 
 
-// add move instruction and short-circuit boolean logic
 
+# Intel
 
 I recommend that your loader call svmdebug_value("unparse") to see if unparsing 
 is requested, and if so, have get_instruction call printasm on every 
 instruction that the loader reads.
 
 
-Quotes
+# Random
+
+If you want a rant on the topic, ask me. re: parsing. Maybe?
+
+# Old code
+            // case InitConsCell: {
+            //     struct VMBlock *vmb = vmalloc_raw(sizeof(*vmb));
+            //     vmb->nslots = 0;
+
+            // }
+
+
+
+# Quotes
 
 "All of the words you said sounded sensible, but there were so many of them 
 I can't hold them all in my head at once."
---
+++ src/svm/vmrun-big.c
@@ -0,0 +1,398 @@
+// Heart of the VM: runs instructions until told to halt
+
+// You'll write a small `vmrun` function in module 1.  You'll pay
+// some attention to performance, but you'll implement only a few 
+// instructions.  You'll add other instructions as needed in future modules.
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "check-expect.h"
+#include "iformat.h"
+#include "value.h"
+#include "vmstate.h"
+#include "vmrun.h"
+
+#include "print.h"
+
+#include "svmdebug.h"
+#include "disasm.h"
+
+#include "vmerror.h"
+#include "vmheap.h"
+#include "vmstring.h"
+
+#define CANDUMP 1
+
+void vmrun(VMState vm, struct VMFunction *fun) {
+    
+    if (fun->size < 1) {
+        return;
+    }
+
+    /* Thank you to Norman for this debugging infrastructure */
+    const char *dump_decode = svmdebug_value("decode");
+    const char *dump_call   = svmdebug_value("call");
+    (void) dump_call;  // make it OK not to use `dump_call`
+
+    Instruction *pc = vm->instructions = fun->instructions;
+
+    Instruction curr_instr;
+    Value *registers = vm->registers; 
+    /* registers always = vm->registers + vm->R_window_start */
+    Value v;
+
+    while(1) {
+        curr_instr = *pc;
+
+        if (CANDUMP && dump_decode) {
+            idump(stderr, vm, (int64_t)pc, curr_instr, 0, 
+            registers + uX(curr_instr), 
+            registers + uY(curr_instr), 
+            registers + uZ(curr_instr));
+        }
+        switch (opcode(curr_instr)) {
+            /* BASIC */
+            case Halt:
+                /* I'm ok with having a counter variable external to the vmstate
+                   and storing it when we Halt, because it saves a dereference
+                   every time, and we only Halt once. */
+                vm->pc = pc;
+                return;
+            case Print:
+                print("%v", registers[uX(curr_instr)]);
+                break;
+            case Println:
+                print("%v\n", registers[uX(curr_instr)]);
+                break;
+            case Printu:
+                print("%U", (unsigned)AS_NUMBER(vm, registers[uX(curr_instr)]));
+                break;     
+            case Error:
+                runerror_p(vm, "%v", registers[uX(curr_instr)]);
+                break;                              
+            case Check: {
+                v = literal_value(vm, uYZ(curr_instr));
+                check(vm, AS_CSTRING(vm, v), registers[uX(curr_instr)]);
+                break;
+            }
+            case Expect: {
+                v = literal_value(vm, uYZ(curr_instr));
+                expect(vm, AS_CSTRING(vm, v), registers[uX(curr_instr)]);
+                break;
+            }
+            case CheckAssert: {
+                v = literal_value(vm, uYZ(curr_instr));
+                check(vm, AS_CSTRING(vm, v), registers[uX(curr_instr)]);
+                expect(vm, AS_CSTRING(vm, v), mkBooleanValue(false));
+                break;
+            }
+
+            /* ARITH- R3 */
+            case Add:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(AS_NUMBER(vm, registers[uY(curr_instr)]) 
+                                  + AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                                                            
+                break;
+            case Sub:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(AS_NUMBER(vm, registers[uY(curr_instr)]) 
+                                  - AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                break;
+            case Mult:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(AS_NUMBER(vm, registers[uY(curr_instr)]) 
+                                  * AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                break;
+
+            case Div: {
+                uint8_t rZ = uZ(curr_instr);
+                Number_T d = AS_NUMBER(vm, registers[rZ]);
+                if (d == 0) {
+                    runerror(vm, "division by zero");
+                }
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(AS_NUMBER(vm, registers[uY(curr_instr)]) / d);
+                break;
+            }
+            // Special case: need to cast to int64_t for idiv && mod since they
+            // have different behavior/are not defined on Number_T (double), 
+            // then cast back to Number_T for mkNumberValue. 
+
+            case IDiv: {
+                uint8_t rZ = uZ(curr_instr);
+                Number_T d = (int64_t)AS_NUMBER(vm, registers[rZ]);
+                if (d == 0) {
+                    runerror(vm, "division by zero");
+                }
+                registers[uX(curr_instr)] = 
+                    mkNumberValue((int64_t)AS_NUMBER(vm, 
+                                                    registers[uY(curr_instr)]) 
+                                            / d);
+                break;
+            }
+            case Mod: {
+                uint8_t rZ = uZ(curr_instr);
+                // coersion
+                int64_t d = AS_NUMBER(vm, registers[rZ]);
+                if (d == 0) {
+                    runerror(vm, "division by zero");
+                }
+                registers[uX(curr_instr)] = 
+                    mkNumberValue((int64_t)AS_NUMBER(vm, 
+                                                    registers[uY(curr_instr)]) 
+                                            % d);
+                break;
+            }
+
+            /* BOOLEAN LOGIC- R3 */
+            case Eq:
+                registers[uX(curr_instr)] = 
+                mkBooleanValue(
+                    eqvalue(registers[uY(curr_instr)], 
+                            registers[uZ(curr_instr)]));
+                break;
+
+            case Gt:
+                registers[uX(curr_instr)] = 
+                mkBooleanValue(AS_NUMBER(vm, registers[uY(curr_instr)]) > 
+                            AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                break;
+
+            case Lt:
+                registers[uX(curr_instr)] = 
+                mkBooleanValue(AS_NUMBER(vm, registers[uY(curr_instr)]) <
+                            AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                break;
+
+            case Ge:
+                registers[uX(curr_instr)] = 
+                mkBooleanValue(AS_NUMBER(vm, registers[uY(curr_instr)]) >= 
+                            AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                break;
+            
+            case Le:
+                registers[uX(curr_instr)] = 
+                mkBooleanValue(AS_NUMBER(vm, registers[uY(curr_instr)]) <=
+                            AS_NUMBER(vm, registers[uZ(curr_instr)]));
+                break;
+
+            /* UNARY ARITH- R1 */
+            case Inc:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(AS_NUMBER(vm, registers[uX(curr_instr)]) + 1);
+                break;
+            case Dec:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(AS_NUMBER(vm, registers[uX(curr_instr)]) - 1);
+                break;
+            case Neg:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(-AS_NUMBER(vm, registers[uX(curr_instr)]));
+                break;    
+            // REV: Is this needed in vScheme?   
+            case Not:
+                registers[uX(curr_instr)] = 
+                    mkNumberValue(~(int64_t)AS_NUMBER(
+                                                vm, registers[uX(curr_instr)]));
+                break;       
+                                          
+            /* LITS <-> GLOBS <-> REGS */
+            case LoadLiteral: 
+                registers[uX(curr_instr)] = vm->literals[uYZ(curr_instr)];
+                break;
+            
+            case GetGlobal:
+                registers[uX(curr_instr)] = vm->globals[uYZ(curr_instr)];
+                break;
+
+            case SetGlobal:
+                vm->globals[uYZ(curr_instr)] = registers[uX(curr_instr)];
+                break;
+
+            /* R2 */
+            case BoolOf:
+                v.tag = Boolean;
+                v.b = asBool(registers[uY(curr_instr)]);
+                registers[uX(curr_instr)] = v;
+                break;
+            
+
+            case RegCopy:
+                registers[uX(curr_instr)] = registers[uY(curr_instr)];
+                break;
+
+            case Swap:
+                v = registers[uX(curr_instr)];
+                registers[uX(curr_instr)] = registers[uY(curr_instr)];
+                registers[uY(curr_instr)] = v;
+                break;
+            
+            case Hash:
+                registers[uX(curr_instr)] = 
+                            mkNumberValue(hashvalue(registers[uY(curr_instr)]));
+                break;
+
+            case PlusImm:
+                registers[uX(curr_instr)] = mkNumberValue(
+                            AS_NUMBER(vm, registers[uY(curr_instr)]) 
+                            + uZ(curr_instr));
+                break;
+            
+
+            /* (UN)CONDITIONAL MOVEMENT */
+            case If: 
+                bool truth = asBool(registers[uX(curr_instr)]);
+                if (!truth) {
+                    pc++; // If false, skip next instruction.
+                }
+                break;
+            case Goto:
+                pc += 1 + iXYZ(curr_instr); 
+                continue; // follows the semantics by adding 1 + for the normal
+                          // counter increment, then adding the goto value, 
+                          // then skipping the increment with continue
+
+
+            /* FUNCTIONS */
+            case Return: {
+                if (vm->stackpointer == 0) {
+                    runerror(vm, "attempting to return register %hhu, "
+                                 "from non-function.", uX(curr_instr));
+                }
+
+                Activation a = vm->Stack[--vm->stackpointer];
+
+                Value return_value = registers[uX(curr_instr)];
+
+                // restore register window state and current instruction
+                vm->R_window_start = a.R_window_start;
+                registers = vm->registers + a.R_window_start;
+                pc = a.resume_loc;
+
+                // set final return
+                registers[a.dest_reg_idx] = return_value;
+                break;
+            }
+
+            case Call: {
+                uint8_t r0 = uY(curr_instr);
+                uint8_t rn = uZ(curr_instr);
+                uint8_t n  = rn - r0;
+
+                uint32_t dest_reg_idx = uX(curr_instr);
+
+
+                // check for invalid function 
+                if (registers[r0].tag == Nil) {
+                    const char *funname = lastglobalset(vm, r0, fun, pc);
+                    if (funname == NULL) {
+                        runerror(vm, 
+                        "tried calling a function in register %hhu, \
+                        which is nil and was never set to a function.\n", r0);
+                    } else {
+                        runerror(vm, 
+                        "tried calling a function in register %hhu, which is \
+                          nil and was last set to function %s.\n", r0, funname);
+                    }
+                }
+                // We'd like to do this up top, but we need to make sure the 
+                // function exists first so we can print a helpful 
+                // debug message with a name we know to be valid!
+                struct VMFunction *func = AS_VMFUNCTION(vm, registers[r0]);
+                if (vm->stackpointer == MAX_STACK_FRAMES) {
+                    fprintf(stderr, "Offending function:");
+                    fprintfunname(stderr, vm, mkVMFunctionValue(func));
+                    runerror(vm, 
+                        "attempting to call function in register %hhu"
+                        " caused a Stack Overflow", r0);
+                }
+
+
+                if (vm->R_window_start + r0 + func->nregs > NUM_REGISTERS) {
+                    fprintf(stderr, "Offending function:");
+                    fprintfunname(stderr, vm, mkVMFunctionValue(func));
+                    runerror(vm, 
+                        "attempting to call function in register %hhu"
+                        " caused a Register Window Overflow", r0);
+                }
+
+                // call stack save
+                Activation a = {pc, vm->R_window_start, dest_reg_idx};
+                vm->Stack[vm->stackpointer++] = a;
+
+
+                // fprintf(stderr, "reg of func: %hhu, arity: %d, n: %hhu\n", r0, func->arity, n);
+
+                assert(func->arity == n);
+
+                // move the register window
+                vm->R_window_start += r0;
+                registers = vm->registers + vm->R_window_start;
+
+                // transfer control= move instruction pointer to start of 
+                // function instruction stream
+                pc = &func->instructions[0] - 1; /* account for increment */
+
+                // return will undo this based on the activation! 
+                break;
+            }
+            case Tailcall: {
+                uint8_t r0 = uX(curr_instr);
+                uint8_t rn = uY(curr_instr);
+                uint8_t n  = rn - r0;
+
+                if (registers[r0].tag == Nil) {
+                    const char *funname = lastglobalset(vm, r0, fun, pc);
+                    if (funname == NULL) {
+                        runerror(vm, 
+                        "tried tailcalling a function in register %hhu, "
+                        "which is nil and was never set to a function.", r0);
+                    } else {
+                        runerror(vm, 
+                    "tried tailcalling a function in register %hhu, which is "
+                    "nil and was last set to function \"%s\".", r0, funname);
+                    }
+                }
+
+                struct VMFunction *func = AS_VMFUNCTION(vm, registers[0]);
+
+                if (rn + vm->R_window_start >= NUM_REGISTERS) {
+                    fprintf(stderr, "Offending function:");
+                    fprintfunname(stderr, vm, mkVMFunctionValue(func));
+                    runerror(vm, 
+                      "attempting to tailcall function in register %hhu"
+                      "caused a Register Window Overflow", r0);
+                }
+
+                // copy over function and argument registers 
+                for (int i = 0; i <= n; ++i) {
+                    registers[i] = registers[r0 + i];
+                }
+
+                // fprintf(stderr, "reg of func: %hhu, arity: %d, n: %hhu\n", r0, func->arity, n);
+                assert(func->arity == n);
+
+                pc = &func->instructions[0] - 1; /* account for increment */
+
+
+                break;
+            }
+
+            default:
+                printf("Opcode Not implemented!\n");
+                break;
+        }
+        pc += 1;
+    }
+    return;
+}
+
