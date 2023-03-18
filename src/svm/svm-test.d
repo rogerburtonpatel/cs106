f5b79e8cf815f02534517f01cb656a4b0208f68e
Roger Burtonpatel <roger.burtonpatel@tufts.edu>
2023-03-17 21:53:57 -0400

    biggen up them nums:

---
+++ src/svm/svm-test.d
@@ -0,0 +1,29 @@
+svm-test.o: svm-test.c check-expect.h value.h iformat.h opcode.h \
+  vmstring.h testfuns.h print.h vmheap.h vmrun.h vmstate.h vmstack.h \
+  name.h vtable.h
+
+check-expect.h:
+
+value.h:
+
+iformat.h:
+
+opcode.h:
+
+vmstring.h:
+
+testfuns.h:
+
+print.h:
+
+vmheap.h:
+
+vmrun.h:
+
+vmstate.h:
+
+vmstack.h:
+
+name.h:
+
+vtable.h:
